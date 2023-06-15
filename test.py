79d31f9eb67228675bd41e50ce7ef84a5a6b7c59
Sheron4ik <sheronov.d@mail.ru>
2023-06-15 22:38:57 +0300

    update test.py

--- test.py
+++ test.py
@@ -75,3 +75,4 @@ v = ".".join(python_version().split(".")[:2])
 with open("picture_" + v + ".txt", "w", encoding="utf-8", errors="ignore") as file:
     file.write("О, мой бог, хорошая работа, Олег!\n")
     file.writelines(pict)
+print(*pict)
